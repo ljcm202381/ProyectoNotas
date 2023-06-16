5fe3bfdba9af06794a8aab2784fac804a9f13462
JOhanna Martinez <cifuentes0903@gmail.com>
2023-06-16 11:45:03 -0500

    Crud de administrador-modelo

--- Administrador/modelos/administrador.php
+++ Administrador/modelos/administrador.php
@@ -1,7 +1,94 @@
 <?php
+include_once('../../Conexion.php');
 
-class Administrador
+class Administrador extends Conexion
 {
+
+	public function __construct()
+	{
+		$this->db = parent::__construct();
+	}
+
+	//funcion para registrar usuarios
+
+	public function addusu()
+	{
+       $statement =$this->db->prepare("INSERT INTO usuarios(Nombreusu, Apellidousu, Usuario, Password, Perfil, Estado) VALUES(:Nombreusu, :Apellidousu, :Usuarioad, :Passwordad,'Administrador','Activo')");
+		$statement->binParam(':Nombreusu',$Nombreusu);
+		$statement->binParam(':Apellidousu',$Apellidousu);
+		$statement->binParam(':Usuarioad',$Usuarioad);
+		$statement->binParam(':Passwordad',$Passwordad);
+		if($statement->execute()){
+			header('Location: ../pages/index.php'):
+		}else
+		{
+			header('Location: ../pages/agregar.php');
+		}
+	}
+
+	//funcion para listar todos los usuarios
+
+	public function getusu()
+	{
+		$row = null;
+		$statement=$this->db->prepare("SELECT * FROM usuarios WHERE Perfil='Administrador'");
+		$statement->execute();
+		while($resul = $statement->fetch())
+		{
+          $row[]=$resul;
+		}
+		return $row;
+
+	}
+    //funcion para listar por id especifico
+	public function getid($Id)
+	{
+		$row = null;
+		$statement=$this->db->prepare("SELECT * FROM usuarios WHERE Perfil='Administrador' AND id_usuario=:Id");
+		$statement->binParam(':Id',$Id);
+		$statement->execute();
+		while($resul = $statement->fetch())
+		{
+          $row[]=$resul;
+		}
+		return $row;
+
+	}
+
+	//funcion para actualizar los usuarios
+	public function updateusu($Id, $Nombreusu, $Apellidousu, $Usuarioad, $Passwordad, $Estadousu)
+	{
+      $statement=$this->db->prepare("UPDATE usuarios SET Nombreusu= :Nombreusu, Apellidousu= :Nombreusu, Usuario= :Usuarioad, Password= :Passwordad, Estado= :Estadousu WHERE id_usuario = $Id");
+        $statement->binParam(':Id',$Id);
+        $statement->binParam(':Nombreusu',$Nombreusu);
+		$statement->binParam(':Apellidousu',$Apellidousu);
+		$statement->binParam(':Usuarioad',$Usuarioad);
+		$statement->binParam(':Passwordad',$Passwordad);
+		$statement->binParam(':Estadousu',$Estadousu);
+		if($statement->execute()){
+			header('Location: ../pages/index.php'):
+		}else
+		{
+			header('Location: ../pages/editar.php');
+		}
+
+	}
+
+	//funcion para eliminar los usuarios
+	public function deleteusu($Id)
+	{
+	$statement=$this->db->prepare("DELETE FROM usuarios WHERE id_usuario=:Id");
+	$statement->binParam(':Id',$Id);
+	if($statement->execute()){
+			header('Location: ../pages/index.php'):
+		}else
+		{
+			header('Location: ../pages/eliminar.php');
+		}
+
+	}
+
+
 	
 }
 
--- Docentes/modelos/docentes.php
+++ Docentes/modelos/docentes.php
@@ -1,6 +1,47 @@
 <?php
-class Docentes
+include_once('../../Conexion.php');
+class Docentes extends Conexion
 {
+
+	public function __construct()
+	{
+		$this->db = parent::__construct();
+	}
+
+	//funcion para registrar docentes
+
+	public function adddoc($Nombreusu,$Apellidousu,$Usuarioad,$Passwordad)
+	{
+		
+
+	}
+
+	//funcion para listar todos los docentes
+
+	public function getdoc()
+	{
+		$
+
+	}
+    //funcion para listar por id especifico
+	public function getiddoc()
+	{
+
+	}
+
+	//funcion para actualizar los docentes
+	public function updatdoc()
+	{
+
+	}
+
+	//funcion para eliminar los usuarios
+	public function deletedoc()
+	{
+		
+	}
+
+
 	
 }
 
